359735561e563db6ab4d36194e9fdbadc2994e31
tsikabullrat <tsika1996190514@gmail.com>
2023-07-05 16:02:48 +0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import  { Nav } from "./comp"
 import { Landing, Home, About, Contacts, News, Store, Learning } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { gsap } from "gsap"
 import "./App.css"
 
 function App() {
-  return <Landing />
+  return (
+    <Router>
+      <Nav />
+      <Routes >
+        <Route path="/" component={Landing}/>
+        <Route path="/home" component={Home}/>
+      </Routes>
+      
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/comp/Navigation.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+export const Nav = () =>{
+    return(
+        <div>
+            <div className="nav-logo">
+                <Link to="/">
+                    
+                </Link>
+            </div>
+            <nav>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+            </nav>
+        </div>
+    )
+}
